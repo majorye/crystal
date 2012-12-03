1508e36667ab24962afb580136b309e9ab267ecd
zhouquan.yezq <xy@xy.local>
2012-12-03 09:38:19 +0800

    add doc dir

---
+++ lib/core/doc/tips.php
@@ -0,0 +1,6 @@
+<?php
+ sleep(0.1); //sleep 10s
+
+echo json_encode("{'content':'sssss'}");
+//echo 200
+?>
---
+++ lib/core/doc/tips2.php
@@ -0,0 +1,6 @@
+<?php
+ sleep(1); //sleep 10s
+
+echo json_encode("{'content':'sssss'}");
+//echo 200
+?>
